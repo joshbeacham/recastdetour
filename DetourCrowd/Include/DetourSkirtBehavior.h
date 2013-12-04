6f4af0517b995755d458b18db82a5b42249b3181
Joel Joly <jjy@masagroup.net>
2013-12-04 11:29:04 +0100

    Add skirt behavior
    this behavior is specialzed on avoiding still obstacles (other agents)
    and circumvent them.

---
+++ DetourCrowd/Include/DetourSkirtBehavior.h
@@ -0,0 +1,96 @@
+//
+// Copyright (c) 2013 MASA Group recastdetour@masagroup.net
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+//
+
+#ifndef DETOURSKIRTBEHAVIOR_H
+#define DETOURSKIRTBEHAVIOR_H
+
+#include "DetourSteeringBehavior.h"
+
+struct dtCrowdAgent;
+
+/// Neighbor agent represented as an obstacle
+struct dtAgentObstacle
+{
+	float position[3];
+	float direction[3];         ///< Normalized vector from the obstacle to the agent
+	float radius;
+};
+/// Navmesh sgment represented as an obstacle
+struct dtSegmentObstacle
+{
+	float closest[2];
+};
+
+/// Defines the skirting behavior.
+///
+/// An agent using this behavior will try to skirt around (circumvent) still targets.
+/// @ingroup behavior
+class dtSkirtBehavior : public dtSteeringBehavior<>
+{
+public:
+	/// Creates an instance of the behavior
+	///
+	/// @param[in]	nbMaxAgents		Estimation of the maximum number of agents using this behavior
+	dtSkirtBehavior(unsigned nbMaxAgents);
+	~dtSkirtBehavior();
+
+	/// Creates an instance of the behavior
+	///
+	/// @param[in]	nbMaxAgents		Estimation of the maximum number of agents using this behavior
+	///
+	/// @return		A pointer on a newly allocated behavior
+	static dtSkirtBehavior* allocate(unsigned nbMaxAgents);
+
+	/// Frees the given behavior
+	///
+	/// @param[in]	ptr	A pointer to the behavior we want to free
+	static void free(dtSkirtBehavior* ptr);
+
+	float distance;             ///< The distance in which other entities are considered
+	float maximumForce;         ///< The maximum repulsion force to be used when agents are near contact
+
+	virtual void computeForce(const dtCrowdQuery& query, const dtCrowdAgent& ag, float* force,
+							  const NoData& currentParams, NoData& newParams);
+
+public:
+	virtual void doUpdate(const dtCrowdQuery& query, const dtCrowdAgent& oldAgent, dtCrowdAgent& newAgent,
+						  const NoData& currentParams, NoData& newParams, float dt);
+
+private:
+	bool updateObtacles(const dtCrowdAgent& ag, const dtCrowdQuery& query);
+	/// Adds a agent obstacle to the obstacles list.
+	///
+	/// @param[in]		agent	The current agent.
+	/// @param[in]		obtacle	The agent to avoid.
+	bool addAgentObstacle(const dtCrowdAgent& agent, const dtCrowdAgent& obtacle);
+
+	/// Adds a segment to the obstacles list.
+	///
+	/// @param[in]	p	The start position of the segment.
+	/// @param[in]	q	The end position of the segment.
+	bool addSegment(const float* p, const float* q);
+
+private:
+	dtAgentObstacle m_agentObstacle;    ///< The closest agent obstacle.
+	float m_agentObstacleDistance;
+	dtSegmentObstacle m_segmentObstacle;
+	float m_segmentObstacleDistance;
+	float currentDt;
+};
+
+#endif
---
+++ DetourCrowd/Source/DetourSkirtBehavior.cpp
@@ -0,0 +1,216 @@
+//
+// Copyright (c) 2013 MASA Group recastdetour@masagroup.net
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+//
+
+#include "DetourSkirtBehavior.h"
+
+#include "DetourCrowd.h"
+#include "DetourCommon.h"
+
+#include <new>
+#include <limits>
+
+dtSkirtBehavior::dtSkirtBehavior(unsigned nbMaxAgents)
+	: dtSteeringBehavior<NoData>(nbMaxAgents)
+	, distance(1.0f)
+	, maximumForce(2.0f)
+	//, m_obstacles(0)
+	//, m_obstaclesCount(0)
+	, m_agentObstacleDistance(std::numeric_limits<float>::max())
+	, m_segmentObstacleDistance(std::numeric_limits<float>::max())
+	, currentDt(0)
+{
+}
+
+dtSkirtBehavior::~dtSkirtBehavior()
+{
+}
+
+dtSkirtBehavior* dtSkirtBehavior::allocate(unsigned nbMaxAgents)
+{
+	void* mem = dtAlloc(sizeof(dtSkirtBehavior), DT_ALLOC_PERM);
+
+	if (mem)
+		return new(mem) dtSkirtBehavior(nbMaxAgents);
+
+	return 0;
+}
+
+void dtSkirtBehavior::free(dtSkirtBehavior* ptr)
+{
+	if (!ptr)
+		return;
+
+	ptr->~dtSkirtBehavior();
+	dtFree(ptr);
+}
+
+void dtSkirtBehavior::computeForce(const dtCrowdQuery&, const dtCrowdAgent& ag, float* force,
+								   const NoData&, NoData&)
+{
+	float currentToAgent[2];
+	currentToAgent[0] = m_agentObstacle.position[0];
+	currentToAgent[1] = m_agentObstacle.position[2];
+	currentToAgent[0] -= ag.position[0];
+	currentToAgent[1] -= ag.position[2];
+	float relativeSide = 0;
+	// check if there is an obstacle segment close to the agent obstacle that is close enough to make moves difficult
+	if (m_segmentObstacleDistance < ag.radius * 2 + m_agentObstacle.radius)
+	{
+		// choose to go at the opposite of the segment obstacle
+		float agentToSegment[2];
+		agentToSegment[0] = m_segmentObstacle.closest[0];
+		agentToSegment[1] = m_segmentObstacle.closest[1];
+		agentToSegment[0] -= m_agentObstacle.position[0];
+		agentToSegment[1] -= m_agentObstacle.position[2];
+		// if positive, the angle from currentToAgent to agentToSegment is positive, agentToSegment is on the left side otherwise agentToSegment is on the right side.
+		relativeSide = currentToAgent[1]*agentToSegment[0] - currentToAgent[0]*agentToSegment[1];
+	}
+	else
+	{
+		// use the shorter path
+		// if positive, the angle from ag.desiredVelocity to currentToAgent is positive, currentToAgent is on the left side otherwise currentToAgent is on the right side.
+		relativeSide = ag.desiredVelocity[2]*currentToAgent[0] - ag.desiredVelocity[0]*currentToAgent[1];
+	}
+	if (relativeSide < 0)
+	{
+		// perpendicular vector pointing to the right
+		force[0] = currentToAgent[1];
+		force[2] = -currentToAgent[0];
+	}
+	else
+	{
+		// perpendicular vector pointing to the left
+		force[0] = -currentToAgent[1];
+		force[2] = currentToAgent[0];
+	}
+
+	// force direction has been computed, now compute its strength based on the normalized distance from agent to obstacle
+	float desiredForce = (this->distance - m_agentObstacleDistance) / this->distance * this->maximumForce;
+	float currentForce = dtSqrt(dtSqr(force[0]) + dtSqr(force[2]));
+	// this can happen when the agent obstacle is precisely on the segment, in such case normal avoidance will be enough
+	if (currentForce > EPSILON)
+	{
+		force[0] *= desiredForce/currentForce;
+		force[2] *= desiredForce/currentForce;
+	}
+	float previousBehaviorForce[3];
+	dtVsub(previousBehaviorForce, ag.desiredVelocity, ag.velocity);
+	dtVscale(previousBehaviorForce, previousBehaviorForce, 1.f/currentDt);
+	dtVadd(force, force, previousBehaviorForce);
+}
+
+void dtSkirtBehavior::doUpdate(const dtCrowdQuery& query, const dtCrowdAgent& oldAgent, dtCrowdAgent& newAgent,
+                               const NoData& currentParams, NoData& newParams, float dt)
+{
+	// only apply the behavior if the agent is trying to move
+	if (dt > EPSILON && dtVlen(oldAgent.desiredVelocity) > EPSILON)
+	{
+		currentDt = dt;
+		// check that we have obstacles
+		if (updateObtacles(oldAgent, query))
+		{
+			// only apply behavior if there is a reason to
+			dtSteeringBehavior<>::doUpdate(query, oldAgent, newAgent, currentParams, newParams, dt);
+		}
+	}
+}
+
+bool dtSkirtBehavior::updateObtacles(const dtCrowdAgent& ag, const dtCrowdQuery& query)
+{
+	m_agentObstacleDistance = m_segmentObstacleDistance = std::numeric_limits<float>::max();
+	const dtCrowdAgentEnvironment* agEnv = query.getAgentEnvironment(ag.id);
+	bool hasObstacles = false;
+	// Add neighbours as obstacles.
+	for (unsigned j = 0; j < agEnv->nbNeighbors; ++j)
+	{
+		const dtCrowdAgent& neighbor = *query.getAgent(agEnv->neighbors[j].idx);
+		hasObstacles = addAgentObstacle(ag, neighbor) || hasObstacles;
+	}
+
+	if (m_agentObstacleDistance != std::numeric_limits<float>::max())
+	{
+		// Append neighbour segments as obstacles.
+		for (int j = 0; j < agEnv->boundary.getSegmentCount(); ++j)
+		{
+			const float* s = agEnv->boundary.getSegment(j);
+
+			// check that the segment is from an outer bound
+			// allows to cross this segment if the current position is inside the obstacle
+			if (dtTriArea2D(ag.position, s, s+3) < 0.f)
+				continue;
+			// do not consider segments that the agent obstacle is within
+			if (dtTriArea2D(m_agentObstacle.position, s, s+3) < 0.f)
+				continue;
+
+			hasObstacles = addSegment(s, s+3) || hasObstacles;
+		}
+	}
+	return hasObstacles;
+}
+
+bool dtSkirtBehavior::addAgentObstacle(const dtCrowdAgent& agent, const dtCrowdAgent& obtacle)
+{
+	float diff[3];
+	dtVsub(diff, obtacle.position, agent.position);
+	float dist = dtVlen(diff) - agent.radius - obtacle.radius;
+
+	if (dist < agent.perceptionDistance && dist <= this->distance && dist <= m_agentObstacleDistance)
+	{
+		// check if the obtacle can be considered still compared to current agent
+		float obstacleVelocity = dtVlenSqr(obtacle.desiredVelocity);
+		if (obstacleVelocity < EPSILON || dtVlenSqr(agent.desiredVelocity) >= obstacleVelocity * 100) // |agent.dvelocity| >= |obstacle.dvelocity| * 10
+		{
+			bool isInTheWay = true;
+			float normalizedDiff[3];
+			dtVcopy(normalizedDiff, diff);
+			normalizedDiff[1] = 0;
+			float normalizedDiffNorm = dtVlen(normalizedDiff);
+			if (normalizedDiffNorm > EPSILON)
+			{
+				dtVscale(normalizedDiff, normalizedDiff, 1.f/dtVlen(normalizedDiff));
+				float normalizedVelocity[3];
+				dtVscale(normalizedVelocity, agent.desiredVelocity, 1.f/dtVlen(agent.desiredVelocity));
+				float cosAngle = dtVdot2D(normalizedDiff, normalizedVelocity);
+				isInTheWay = acos(cosAngle) <= 90.f/360.f*3.14159265f;
+			}
+			if (isInTheWay)
+			{
+				m_agentObstacleDistance = dist;
+				dtVcopy(m_agentObstacle.position, obtacle.position);
+				dtVcopy(m_agentObstacle.direction, normalizedDiff);
+				m_agentObstacle.radius = obtacle.radius;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool dtSkirtBehavior::addSegment(const float* p, const float* q)
+{
+	float projectionFator;
+	float distanceToSegment = dtDistancePtSegSqr2D(m_agentObstacle.position, p, q, projectionFator);
+	if (distanceToSegment < m_segmentObstacleDistance)
+	{
+		m_segmentObstacleDistance = distanceToSegment;
+		m_segmentObstacle.closest[0] = p[0] + (q[0] - p[0]) * projectionFator;
+		m_segmentObstacle.closest[1] = p[2] + (q[2] - p[2]) * projectionFator;
+		return true;
+	}
+	return false;
+}
